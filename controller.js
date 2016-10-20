8e19cf16b6511697329c9000a9af1dfadf2e45c7
Henry Qi <henryqi222@gmail.com>
2016-10-20 14:48:06 -0600

    completed instructions

--- controller.js
+++ controller.js
@@ -1,10 +1,11 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
+  $scope.getUsers = function(){
+		mainService.getUsers().then(function(response){
+			$scope.users = response.data.data;
+		})
+	}
 
-
-  $scope.getUsers();
-
+	$scope.getUsers();
+	
 });
